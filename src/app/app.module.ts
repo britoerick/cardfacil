82d745915408582f7019cc2b55d904ed99b8a83c
Erick Brito <erickdebrito@hotmail.com>
2018-05-09 08:16:37 -0300

    recarga crédito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { BrMaskerModule } from 'brmasker-ionic-3';
+
 import { AboutPage } from '../pages/about/about';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { RegisterPage } from './../pages/register/register';
 import { RecargaCreditoPage } from '../pages/recarga-credito/recarga-credito';
 import { RecargaDebitoPage } from '../pages/recarga-debito/recarga-debito';
-
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -26,7 +27,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    BrMaskerModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -36,7 +38,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     RecargaCreditoPage,
     RecargaDebitoPage,
-    TabsPage
+    TabsPage  
   ],
   providers: [
     StatusBar,
